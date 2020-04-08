dc73c928ba7f5684373fbb77b7f1f69ab4ae81bf
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-08 11:34:55 +0200

    set firebase config depending on stage

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -5,7 +5,17 @@ import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const firebaseConfig = {
+const firebaseConfigDev = {
+  apiKey: 'AIzaSyCp05cO6WVDkktHd7rGmGzfrjEAYPgKmAI',
+  authDomain: 'schnelle-stimme-dev.firebaseapp.com',
+  databaseURL: 'https://schnelle-stimme-dev.firebaseio.com',
+  projectId: 'schnelle-stimme-dev',
+  storageBucket: 'schnelle-stimme-dev.appspot.com',
+  messagingSenderId: '811423793597',
+  appId: '1:811423793597:web:8172788fabfcb30a9f52d4',
+};
+
+const firebaseConfigProd = {
   apiKey: 'AIzaSyB8MJIgz14aM9Fe4sXTDtv3kJjOt8lM40o',
   authDomain: 'schnelle-stimme.firebaseapp.com',
   databaseURL: 'https://schnelle-stimme.firebaseio.com',
@@ -13,7 +23,10 @@ const firebaseConfig = {
   storageBucket: 'schnelle-stimme.appspot.com',
   messagingSenderId: '906966557651',
   appId: '1:906966557651:web:5c11bcce86958297e98526',
-  measurementId: 'G-VS2HGVTGE6',
 };
+
+const stage = process.env.REACT_APP_STAGE;
+const firebaseConfig =
+  stage === 'production' ? firebaseConfigProd : firebaseConfigDev;
 
 export const Firebase = app.initializeApp(firebaseConfig);
