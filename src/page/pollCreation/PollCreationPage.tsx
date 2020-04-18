779a876010ae973a18d33626a4a2ad1cf6544d01
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-18 17:03:42 +0200

    implemented headline and topic-input

--- src/page/pollCreation/PollCreationPage.tsx
+++ src/page/pollCreation/PollCreationPage.tsx
@@ -1,5 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+
+import { PollCreationContext } from '../../context/PollCreationContext';
+import { TopicInput } from './TopicInput';
 
 export const PollCreationPage: FC = () => {
-  return <h1>Poll Creation Page</h1>;
+  const { initialTopic } = useContext(PollCreationContext);
+
+  const [topic, setTopic] = useState<string>(initialTopic);
+
+  useEffect(() => {
+    setTopic(initialTopic);
+  }, [initialTopic]);
+
+  return (
+    <Wrapper>
+      <Typography variant="h4" align="center">
+        Umfrage erstellen
+      </Typography>
+      <TopicInput value={topic} onChange={setTopic} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  padding: 2rem 1rem;
+  display: grid;
+  gap: 1rem;
+`;
---
+++ src/page/pollCreation/TopicInput.tsx
@@ -0,0 +1,25 @@
+import React, { FC, useState } from 'react';
+import { TextField } from '@material-ui/core';
+
+interface Props {
+  value: string;
+  onChange: (topic: string) => void;
+}
+
+export const TopicInput: FC<Props> = ({ value, onChange }) => {
+  const [wasTargeted, setWasTargeted] = useState(false);
+
+  return (
+    <TextField
+      id="topic"
+      label="Thema / Frage"
+      variant="outlined"
+      required
+      fullWidth
+      error={wasTargeted && value === ''}
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+      onBlur={() => setWasTargeted(true)}
+    />
+  );
+};
