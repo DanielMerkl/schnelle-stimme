547a9f5931cc2d327c42a57a541d6a89b440d67d
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-19 09:42:45 +0200

    commented out poll types that are not part of the MVP

--- src/utils/function/pollTypeToGerman.ts
+++ src/utils/function/pollTypeToGerman.ts
@@ -1,19 +1,20 @@
 import { PollType } from '../../types/enum/PollType';
 
+// TODO: uncomment cases after/while implementing corresponding features
 export const pollTypeToGerman = (pollType: PollType): string => {
   switch (pollType) {
     case PollType.SINGLE_CHOICE:
       return 'Single-Choice';
     case PollType.MULTIPLE_CHOICE:
       return 'Multiple-Choice';
-    case PollType.OPEN_ENDED:
-      return 'Offene Antwort';
-    case PollType.RANKING:
-      return 'Rangfolge';
-    case PollType.YES_NO:
-      return 'Ja/Nein';
-    case PollType.POINT_ALLOCATION:
-      return 'Punktvergabe';
+    // case PollType.OPEN_ENDED:
+    //   return 'Offene Antwort';
+    // case PollType.RANKING:
+    //   return 'Rangfolge';
+    // case PollType.YES_NO:
+    //   return 'Ja/Nein';
+    // case PollType.POINT_ALLOCATION:
+    //   return 'Punktvergabe';
     default:
       throw new Error(`invalid poll type "${pollType}"`);
   }
