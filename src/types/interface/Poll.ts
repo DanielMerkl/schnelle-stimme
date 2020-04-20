dec3a8bcd6f2a6ede69be1b0ea8b5b948d20cab8
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-20 12:20:08 +0200

    added specific type for a choice

--- src/types/interface/Poll.ts
+++ src/types/interface/Poll.ts
@@ -1,12 +1,13 @@
 import { PollType } from '../enum/PollType';
 import { Answer } from './Answer';
+import { Choice } from './Choice';
 
 export interface Poll {
   id: string;
   invitationCode: number;
   topic: string;
   description?: string;
-  choices: Array<string>;
+  choices: Array<Choice>;
   type: PollType;
   isNameMandatory: boolean;
   isResultSecret: boolean;
