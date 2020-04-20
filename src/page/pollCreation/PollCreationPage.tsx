4e873043aee73addef8cfc0d1172fb1ffd3575cc
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-20 15:15:53 +0200

    added components to edit choices inside PollCreationPage

--- src/page/pollCreation/PollCreationPage.tsx
+++ src/page/pollCreation/PollCreationPage.tsx
@@ -1,11 +1,14 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { TextField, Typography } from '@material-ui/core';
+import { Divider, TextField, Typography } from '@material-ui/core';
 
 import { PollCreationContext } from '../../context/PollCreationContext';
 import { TopicInput } from './TopicInput';
 import { PollType } from '../../types/enum/PollType';
 import { PollTypeSelection } from './PollTypeSelection';
+import { AddAdditionalChoiceInput } from './AddAdditionalChoiceInput';
+import { Choice } from '../../types/interface/Choice';
+import { Choices } from './Choices';
 
 export const PollCreationPage: FC = () => {
   const { initialTopic } = useContext(PollCreationContext);
@@ -13,6 +16,7 @@ export const PollCreationPage: FC = () => {
   const [topic, setTopic] = useState<string>(initialTopic);
   const [description, setDescription] = useState('');
   const [pollType, setPollType] = useState<PollType>(PollType.SINGLE_CHOICE);
+  const [choices, setChoices] = useState<Array<Choice>>([]);
 
   useEffect(() => {
     setTopic(initialTopic);
@@ -20,9 +24,7 @@ export const PollCreationPage: FC = () => {
 
   return (
     <Wrapper>
-      <Typography variant="h4" align="center">
-        Umfrage erstellen
-      </Typography>
+      <Typography variant="h4">Umfrage erstellen</Typography>
       <TopicInput value={topic} onChange={setTopic} />
       <TextField
         id="description"
@@ -33,12 +35,19 @@ export const PollCreationPage: FC = () => {
         onChange={(event) => setDescription(event.target.value)}
       />
       <PollTypeSelection value={pollType} onChange={setPollType} />
+      <Divider />
+      <Typography variant="h5">Antwortmöglichkeiten</Typography>
+      <Choices choices={choices} setChoices={setChoices} />
+      <AddAdditionalChoiceInput choices={choices} setChoices={setChoices} />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.main`
+  height: fit-content;
   padding: 2rem 1rem;
   display: grid;
   gap: 1rem;
+  grid-template-columns: minmax(min-content, 600px);
+  justify-content: center;
 `;
