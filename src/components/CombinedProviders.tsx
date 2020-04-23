b3a9055ab94389f8cc45323f41ebad1c69c6eb60
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-23 18:17:57 +0200

    added global SnackbarContext to show Snackbar from anywhere

--- src/components/CombinedProviders.tsx
+++ src/components/CombinedProviders.tsx
@@ -5,17 +5,20 @@ import { StylesProvider, ThemeProvider } from '@material-ui/core';
 import { theme } from '../utils/theme';
 import { PollCreationContextProvider } from '../context/PollCreationContext';
 import { UserContextProvider } from '../context/UserContext';
+import { SnackbarContextProvider } from '../context/SnackbarContext';
 
 export const CombinedProviders: FC = ({ children }) => {
   return (
     <BrowserRouter>
       <StylesProvider injectFirst>
         <ThemeProvider theme={theme}>
-          <UserContextProvider>
-            <PollCreationContextProvider>
-              {children}
-            </PollCreationContextProvider>
-          </UserContextProvider>
+          <SnackbarContextProvider>
+            <UserContextProvider>
+              <PollCreationContextProvider>
+                {children}
+              </PollCreationContextProvider>
+            </UserContextProvider>
+          </SnackbarContextProvider>
         </ThemeProvider>
       </StylesProvider>
     </BrowserRouter>
