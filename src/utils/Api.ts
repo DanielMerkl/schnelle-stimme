388dd05a8b95bcf64b3b7071ab6ae385f99b3ff9
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-04-24 10:18:19 +0200

    implemented interface to create a poll (WIP)

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,5 +1,8 @@
 import { User } from 'firebase';
+
 import { Firebase } from './Firebase';
+import { Poll } from '../types/interface/Poll';
+import { Collection } from '../types/enum/Collection';
 
 const signIn = async (): Promise<User | null> => {
   try {
@@ -11,6 +14,38 @@ const signIn = async (): Promise<User | null> => {
   }
 };
 
+const createPoll = async (poll: Poll): Promise<Poll> => {
+  try {
+    const invitationCode = await determineInvitationCode();
+    const newDocRef = await Firebase.firestore()
+      .collection(Collection.polls)
+      .doc();
+    const pollWithGeneratedId: Poll = {
+      ...poll,
+      id: newDocRef.id,
+      invitationCode,
+    };
+    newDocRef.set(pollWithGeneratedId);
+
+    return pollWithGeneratedId;
+  } catch (e) {
+    console.error(e);
+    throw new Error('Fehler beim Erstellen der Umfrage.');
+  }
+};
+
+const determineInvitationCode = async (): Promise<number> => {
+  try {
+    // TODO: implement
+    // const invitationCode = Firebase-Function...???
+    return 12345;
+  } catch (e) {
+    console.error(e);
+    throw new Error('Fehler beim generieren des Einladungscodes.');
+  }
+};
+
 export const Api = {
   signIn,
+  createPoll,
 };
