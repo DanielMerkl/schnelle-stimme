63522740bdcc974d8b38f0dd22acf3ce8cbb5fb3
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-03 10:52:44 +0200

    commented out answer-content-types that are not yet necessary

--- src/types/type/AnswerContent.ts
+++ src/types/type/AnswerContent.ts
@@ -1,16 +1,15 @@
 import { SingleChoiceAnswer } from './SingleChoiceAnswer';
 import { MultipleChoiceAnswer } from './MultipleChoiceAnswer';
-import { OpenEndedAnswer } from './OpenEndedAnswer';
-import { RankingAnswer } from './RankingAnswer';
-import { YesNoAnswer } from './YesNoAnswer';
-import { PointAllocationAnswer } from './PointAllocationAnswer';
-import { MatrixAnswer } from './MatrixAnswer';
+// TODO: uncomment after/while implementing corresponding features
+// import { OpenEndedAnswer } from './OpenEndedAnswer';
+// import { RankingAnswer } from './RankingAnswer';
+// import { YesNoAnswer } from './YesNoAnswer';
+// import { PointAllocationAnswer } from './PointAllocationAnswer';
+// import { MatrixAnswer } from './MatrixAnswer';
 
-export type AnswerContent =
-  | SingleChoiceAnswer
-  | MultipleChoiceAnswer
-  | OpenEndedAnswer
-  | RankingAnswer
-  | YesNoAnswer
-  | PointAllocationAnswer
-  | MatrixAnswer;
+export type AnswerContent = SingleChoiceAnswer | MultipleChoiceAnswer;
+// | OpenEndedAnswer
+// | RankingAnswer
+// | YesNoAnswer
+// | PointAllocationAnswer
+// | MatrixAnswer;
