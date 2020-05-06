46b685127a3ed9a414668618522aacd8bf7a9497
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-06 20:06:25 +0200

    implemented tests for SubmitVoteButton

---
+++ src/page/poll/SubmitVoteButton.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { SubmitVoteButton } from './SubmitVoteButton';
+
+describe('SubmitVoteButton', () => {
+  it('renders correct button text', () => {
+    const { getByText } = render(
+      <SubmitVoteButton disabled={false} isLoading={false} onClick={() => {}} />
+    );
+    const button = getByText('abstimmen').parentElement;
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('disables button', () => {
+    const { getByText } = render(
+      <SubmitVoteButton disabled={true} isLoading={false} onClick={() => {}} />
+    );
+    const button = getByText('abstimmen').parentElement;
+
+    expect(button).toBeDisabled();
+  });
+
+  it('renders circular progress when loading', () => {
+    const { getByRole } = render(
+      <SubmitVoteButton disabled={true} isLoading={true} onClick={() => {}} />
+    );
+    const loadingSpinner = getByRole('progressbar');
+
+    expect(loadingSpinner).toBeInTheDocument();
+  });
+
+  it('calls onClick callback', () => {
+    const onClickMock = jest.fn();
+    const { getByText } = render(
+      <SubmitVoteButton
+        disabled={false}
+        isLoading={false}
+        onClick={onClickMock}
+      />
+    );
+    const button = getByText('abstimmen').parentElement ?? new Element();
+
+    userEvent.click(button);
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
