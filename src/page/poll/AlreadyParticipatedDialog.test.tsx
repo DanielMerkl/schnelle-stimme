c5d7d61d79a02ae15582068d33a5f5afd0331174
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-06 19:37:05 +0200

    refactoring

--- src/page/poll/AlreadyParticipatedDialog.test.tsx
+++ src/page/poll/AlreadyParticipatedDialog.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, RenderResult } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { AlreadyParticipatedDialog } from './AlreadyParticipatedDialog';
-import userEvent from '@testing-library/user-event';
 
 describe('AlreadyParticipatedDialog.test', () => {
   let renderResult: RenderResult;
