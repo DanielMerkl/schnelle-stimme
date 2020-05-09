db4f62a66fa9560e24252e24214bb965ef646cd9
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-09 21:16:45 +0200

    added global scroll-to-top functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 import { CombinedProviders } from './components/CombinedProviders';
+import { renderWithProviders } from './utils/function/renderWithProviders';
 
 describe('App', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
@@ -14,5 +19,12 @@ describe('App', () => {
       div
     );
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('scrolls to the top on pathname change', async () => {
+    renderWithProviders(<App />);
+
+    expect(window.scrollTo).toHaveBeenCalledTimes(1);
+    expect(window.scrollTo).toHaveBeenLastCalledWith({ top: 0 });
   });
 });
