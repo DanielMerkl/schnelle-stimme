703e65e923c45e3fb848db868bb1229343900daf
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-09 18:07:28 +0200

    added test verifying that poll page is opened after successful join by invitation code

--- src/page/home/PollJoiningCallToAction.test.tsx
+++ src/page/home/PollJoiningCallToAction.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RenderResult, act, fireEvent } from '@testing-library/react';
+import { act, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { PollJoiningCallToAction } from './PollJoiningCallToAction';
@@ -7,35 +7,35 @@ import { messages } from './utils/messages';
 import { Api } from '../../utils/Api';
 import { renderWithProviders } from '../../utils/function/renderWithProviders';
 import { keyboardEventMock } from '../../utils/keyboardEventMock';
+import { PollType } from '../../types/enum/PollType';
+import { Poll } from '../../types/interface/Poll';
+import { App } from '../../App';
 
 describe('PollJoiningCallToAction', () => {
-  let renderResult: RenderResult;
   const ApiMock = Api as jest.Mocked<typeof Api>;
-
-  beforeEach(() => {
-    renderResult = renderWithProviders(<PollJoiningCallToAction />);
-  });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   it('renders the correct headline', () => {
-    const { getByText } = renderResult;
+    const { getByText } = renderWithProviders(<PollJoiningCallToAction />);
     const headline = getByText('Trete einer Umfrage bei');
 
     expect(headline).toBeInTheDocument();
   });
 
   it('renders a submit button', () => {
-    const { getByText } = renderResult;
+    const { getByText } = renderWithProviders(<PollJoiningCallToAction />);
     const submitButton = getByText('beitreten');
 
     expect(submitButton).toBeInTheDocument();
   });
 
   it('shows error message if code is too short', () => {
-    const { getByLabelText, getByText } = renderResult;
+    const { getByLabelText, getByText } = renderWithProviders(
+      <PollJoiningCallToAction />
+    );
     const codeInput = getByLabelText('5-stelliger Code');
     const submitButton = getByText('beitreten');
 
@@ -48,7 +48,9 @@ describe('PollJoiningCallToAction', () => {
   });
 
   it('hides error message when user is typing', () => {
-    const { getByLabelText, getByText, queryByText } = renderResult;
+    const { getByLabelText, getByText, queryByText } = renderWithProviders(
+      <PollJoiningCallToAction />
+    );
     const codeInput = getByLabelText('5-stelliger Code');
     const submitButton = getByText('beitreten');
 
@@ -66,10 +68,10 @@ describe('PollJoiningCallToAction', () => {
   });
 
   it('shows error message if poll was not found', async () => {
-    ApiMock.findPollByInvitationCode.mockImplementationOnce(() => {
-      return Promise.resolve(null);
-    });
-    const { getByText, getByLabelText } = renderResult;
+    ApiMock.findPollByInvitationCode.mockResolvedValue(null);
+    const { getByText, getByLabelText } = renderWithProviders(
+      <PollJoiningCallToAction />
+    );
     const codeInput = getByLabelText('5-stelliger Code');
     const submitButton = getByText('beitreten');
 
@@ -84,7 +86,9 @@ describe('PollJoiningCallToAction', () => {
   });
 
   it('submits the code after clicking on the submit button', async () => {
-    const { getByText, getByLabelText } = renderResult;
+    const { getByText, getByLabelText } = renderWithProviders(
+      <PollJoiningCallToAction />
+    );
     const codeInput = getByLabelText('5-stelliger Code');
     const submitButton = getByText('beitreten');
 
@@ -97,7 +101,7 @@ describe('PollJoiningCallToAction', () => {
   });
 
   it('submits the code after pressing the enter key', async () => {
-    const { getByLabelText } = renderResult;
+    const { getByLabelText } = renderWithProviders(<PollJoiningCallToAction />);
     const codeInput = getByLabelText('5-stelliger Code');
 
     await act(async () => {
@@ -108,7 +112,34 @@ describe('PollJoiningCallToAction', () => {
     expect(ApiMock.findPollByInvitationCode).toHaveBeenCalledWith(12345);
   });
 
-  xit('opens the poll after joining successfully by invitation code', () => {
-    // TODO: continue after implementing poll page
+  it('opens the poll after joining successfully by invitation code', async () => {
+    const poll: Poll = {
+      id: '123',
+      invitationCode: 11111,
+      topic: 'Banana',
+      description: 'One crooked banana.',
+      choices: [{ id: '1', text: 'Apple' }],
+      type: PollType.SINGLE_CHOICE,
+      isNameMandatory: false,
+      isResultSecret: false,
+      answers: [],
+    };
+    ApiMock.findPollByInvitationCode.mockResolvedValue(poll);
+    const { getByText, getByLabelText } = renderWithProviders(<App />);
+    const codeInput = getByLabelText('5-stelliger Code');
+
+    await act(async () => {
+      await userEvent.type(codeInput, '12345');
+      fireEvent.keyPress(codeInput, keyboardEventMock.enter);
+    });
+
+    const topicHeadline = getByText('Banana');
+    const description = getByText('One crooked banana.');
+    const appleChoice = getByLabelText('Apple');
+    const submitVoteButton = getByText('abstimmen');
+    expect(topicHeadline).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(appleChoice).toBeInTheDocument();
+    expect(submitVoteButton).toBeInTheDocument();
   });
 });
