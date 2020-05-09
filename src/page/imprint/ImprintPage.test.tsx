d148c2c3f8dcdbc72c8b9885914189860be3d3d3
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-09 21:17:59 +0200

    removed redundant scroll-to-top function call in ImprintPage

--- src/page/imprint/ImprintPage.test.tsx
+++ src/page/imprint/ImprintPage.test.tsx
@@ -4,24 +4,9 @@ import { render } from '@testing-library/react';
 import { ImprintPage } from './ImprintPage';
 
 describe('ImprintPage', () => {
-  beforeEach(() => {
-    window.scrollTo = jest.fn();
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   it('renders correctly', () => {
     const { container } = render(<ImprintPage />);
 
     expect(container).toMatchSnapshot();
-  });
-
-  it('scrolls to the top', () => {
-    render(<ImprintPage />);
-
-    expect(window.scrollTo).toHaveBeenCalledTimes(1);
-    expect(window.scrollTo).toHaveBeenCalledWith({ top: 0 });
   });
 });
