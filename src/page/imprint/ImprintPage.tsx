12b31196146e2f1d29e1e7a5044627a5e04e4aa4
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-15 07:59:08 +0200

    removed alignment property from textblocks

--- src/page/imprint/ImprintPage.tsx
+++ src/page/imprint/ImprintPage.tsx
@@ -35,7 +35,7 @@ export const ImprintPage: FC = () => (
       <Typography variant="h6" gutterBottom>
         Haftung für Inhalte
       </Typography>
-      <Typography align="justify" gutterBottom>
+      <Typography gutterBottom>
         Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
         diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
         bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
@@ -43,7 +43,7 @@ export const ImprintPage: FC = () => (
         nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
         hinweisen.
       </Typography>
-      <Typography align="justify">
+      <Typography>
         Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
         Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
         Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
@@ -57,7 +57,7 @@ export const ImprintPage: FC = () => (
       <Typography variant="h6" gutterBottom>
         Haftung für Links
       </Typography>
-      <Typography align="justify" gutterBottom>
+      <Typography gutterBottom>
         Unser Angebot enthält Links zu externen Websites Dritter, auf deren
         Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
         Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
@@ -66,7 +66,7 @@ export const ImprintPage: FC = () => (
         Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte
         waren zum Zeitpunkt der Verlinkung nicht erkennbar.
       </Typography>
-      <Typography align="justify">
+      <Typography>
         Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
         ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
         Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend
@@ -78,7 +78,7 @@ export const ImprintPage: FC = () => (
       <Typography variant="h6" gutterBottom>
         Urheberrecht
       </Typography>
-      <Typography align="justify" gutterBottom>
+      <Typography gutterBottom>
         Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
         Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
         Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
@@ -86,7 +86,7 @@ export const ImprintPage: FC = () => (
         jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
         sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
       </Typography>
-      <Typography align="justify">
+      <Typography>
         Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
         werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
         Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
