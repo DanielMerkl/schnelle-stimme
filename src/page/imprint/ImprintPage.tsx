461f73d820d1e8cea230945f1f9652a3c72bd415
Daniel Merkl <daniel.merkl.dm@gmail.com>
2020-05-15 07:55:53 +0200

    removed default variant "body1" from Typography

--- src/page/imprint/ImprintPage.tsx
+++ src/page/imprint/ImprintPage.tsx
@@ -13,9 +13,9 @@ export const ImprintPage: FC = () => (
       <Typography variant="h5" gutterBottom>
         Adresse
       </Typography>
-      <Typography variant="body1">Max Mustermann</Typography>
-      <Typography variant="body1">Musterstraße 40</Typography>
-      <Typography variant="body1">94242 Musterstadt</Typography>
+      <Typography>Max Mustermann</Typography>
+      <Typography>Musterstraße 40</Typography>
+      <Typography>94242 Musterstadt</Typography>
     </section>
 
     <section>
@@ -25,9 +25,9 @@ export const ImprintPage: FC = () => (
       </Typography>
       <ContactWrapper>
         <Typography>Telefon:</Typography>
-        <Typography variant="body1">+49 (0) 151 42424242</Typography>
-        <Typography variant="body1">E-Mail:</Typography>
-        <Typography variant="body1">mustermann@musterfirma.de</Typography>
+        <Typography>+49 (0) 151 42424242</Typography>
+        <Typography>E-Mail:</Typography>
+        <Typography>mustermann@musterfirma.de</Typography>
       </ContactWrapper>
     </section>
 
@@ -35,7 +35,7 @@ export const ImprintPage: FC = () => (
       <Typography variant="h6" gutterBottom>
         Haftung für Inhalte
       </Typography>
-      <Typography variant="body1" align="justify" gutterBottom>
+      <Typography align="justify" gutterBottom>
         Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
         diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
         bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
@@ -43,7 +43,7 @@ export const ImprintPage: FC = () => (
         nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
         hinweisen.
       </Typography>
-      <Typography variant="body1" align="justify">
+      <Typography align="justify">
         Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
         Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
         Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
@@ -57,7 +57,7 @@ export const ImprintPage: FC = () => (
       <Typography variant="h6" gutterBottom>
         Haftung für Links
       </Typography>
-      <Typography variant="body1" align="justify" gutterBottom>
+      <Typography align="justify" gutterBottom>
         Unser Angebot enthält Links zu externen Websites Dritter, auf deren
         Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
         Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
@@ -66,7 +66,7 @@ export const ImprintPage: FC = () => (
         Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte
         waren zum Zeitpunkt der Verlinkung nicht erkennbar.
       </Typography>
-      <Typography variant="body1" align="justify">
+      <Typography align="justify">
         Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
         ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
         Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend
@@ -78,7 +78,7 @@ export const ImprintPage: FC = () => (
       <Typography variant="h6" gutterBottom>
         Urheberrecht
       </Typography>
-      <Typography variant="body1" align="justify" gutterBottom>
+      <Typography align="justify" gutterBottom>
         Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
         Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
         Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
@@ -86,7 +86,7 @@ export const ImprintPage: FC = () => (
         jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
         sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
       </Typography>
-      <Typography variant="body1" align="justify">
+      <Typography align="justify">
         Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
         werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
         Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
